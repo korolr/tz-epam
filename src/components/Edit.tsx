58f653d89aabf6d8569b7abdec33dd2844c91902
Alexsey <ekstazu@gmail.com>
2020-10-12 11:11:47 +0400

    add edit component

--- src/components/Edit.tsx
+++ src/components/Edit.tsx
@@ -1,10 +1,11 @@
-import React, { FunctionComponent, useEffect } from "react"
+import React, { FunctionComponent, useEffect, useState } from "react"
 import { useForm } from "react-hook-form"
+import { useLocation } from "wouter"
 
 import { Article } from "../reducers/articles"
 import { Error } from "./Error"
 import { toBase64 } from "../util"
-import { editArticle } from "../actions/articlesActions"
+import DatePicker from "react-datepicker"
 
 export interface Props {
   articles: Array<Article>
@@ -21,21 +22,36 @@ export const Edit: FunctionComponent<Props> = ({
   editArticle,
   text,
 }) => {
-  const { register, handleSubmit, errors, setValue } = useForm()
-  const onSubmit = (data: any) => {
-    editArticle(parseInt(text), {
+  const { register, handleSubmit, errors } = useForm()
+  const [, setLocation] = useLocation()
+  const [startDate, setStartDate] = useState<Date>(new Date())
+  const [imageBase, setImageBase] = useState("")
+
+  const onSubmit = (data: { title: string; priview: string; text: string }) => {
+    let id = parseInt(text)
+    editArticle(id, {
       ...data,
-      id: parseInt(text),
+      id: id,
+      image: imageBase,
+      date: startDate.toLocaleDateString("ru-RU"),
       visible: true,
       viewed: false,
     })
+    setLocation("/article/" + id)
   }
 
   const article = articles.filter((article) => article.id === parseInt(text))
 
-  const handleChange = (e: any) => {
-    toBase64(e.target.files[0]).then((data) => {
-      setValue("image", data)
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    article.length >= 1 &&
+      (setStartDate(new Date(article[0].date)), setImageBase(article[0].image))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    toBase64(e.target.files[0]).then((data: string) => {
+      setImageBase(data)
     })
   }
 
@@ -50,44 +66,74 @@ export const Edit: FunctionComponent<Props> = ({
   return (
     <div className="container">
       <div className="row">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            name="title"
-            defaultValue={article[0].title}
-            ref={register({ required: true })}
-          />
+        <form onSubmit={handleSubmit(onSubmit)} className="edit-input">
+          <div>
+            <b>Title:</b>
+            <br />
+            <input
+              name="title"
+              defaultValue={article[0].title}
+              ref={register({ required: true })}
+              className="edit-input"
+            />
+          </div>
           {errors.title && <span>This field is required</span>}
           <br />
-          <input
-            name="date"
-            defaultValue={article[0].date}
-            ref={register({ required: true })}
-          />
-          {errors.date && <span>This field is required</span>}
+          <div>
+            <b>Date:</b>
+            <br />
+            <DatePicker
+              name="date"
+              selected={startDate}
+              dateFormat="dd/MM/yyyy"
+              popperPlacement="top-end"
+              // Плохие биндинги воообще невозможно типизировать под кейс с кастомным инпутом
+              // @ts-ignore TODO Сделать пуллреквест в биндинги*/
+              onChange={(date) => setStartDate(date)}
+            />
+          </div>
           <br />
-          <input
-            name="image"
-            defaultValue={article[0].image}
-            ref={register({ required: true })}
-          />
-          <input name="image-select" type="file" onChange={handleChange} />
-          {errors.image && <span>This field is required</span>}
+          <p>
+            <b>Image:</b>
+            <br />
+            <img
+              src={imageBase}
+              alt="article_img"
+              className="article-img-post"
+            />
+            <input
+              name="image-select"
+              type="file"
+              className="edit-file"
+              onChange={handleChange}
+            />
+          </p>
           <br />
-          <input
-            name="priview"
-            defaultValue={article[0].priview}
-            ref={register({ required: true })}
-          />
-          {errors.priview && <span>This field is required</span>}
+          <div>
+            <b>Priview:</b>
+            <br />
+            <textarea
+              name="priview"
+              defaultValue={article[0].priview}
+              ref={register({ required: true })}
+              className="edit-input edit-input_area"
+            />
+            {errors.priview && <span>This field is required</span>}
+          </div>
           <br />
-          <input
-            name="text"
-            defaultValue={article[0].text}
-            ref={register({ required: true })}
-          />
-          {errors.text && <span>This field is required</span>}
+          <div>
+            <b>Text:</b>
+            <br />
+            <textarea
+              name="text"
+              defaultValue={article[0].text}
+              ref={register({ required: true })}
+              className="edit-input edit-input_area"
+            />
+            {errors.text && <span>This field is required</span>}
+          </div>
           <br />
-          <input type="submit" />
+          <input type="submit" value={text === "add" ? "Add" : "Edit"} />
         </form>
       </div>{" "}
     </div>
